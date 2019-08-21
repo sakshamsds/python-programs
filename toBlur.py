350d8bb99cf0a25d3398c319a8d44b0ddd7b3630
Sakshamdeep Singh <saksham.sds@gmail.com>
2019-08-21 14:55:42 +0530

    Update toBlur.py

--- toBlur.py
+++ toBlur.py
@@ -1,11 +1,10 @@
-#????????
 # -*- coding: utf-8 -*-
 
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 
-img = mpimg.imread('ClearImg.jpg')
+img = mpimg.imread('sample.jpg')
 plt.imshow(img)
 
 # Parameters 
@@ -14,49 +13,18 @@ n_H = img.shape[0]
 n_W = img.shape[1] 
 n_C = img.shape[2]
 stride = 1
-padding = 1
+pad = int((f-1)/2) 
 
 # filter
-blur_filter = np.ones((f,f,n_C), np.float)/(f*f*n_C)
+blur_filter = np.ones((f,f), np.float)/(f*f)
+#vertical_edge_detector = np.array([[1,0,-1],[1,0,-1],[1,0,-1]])
+#vertical_edge_detector = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+#horizontal_edge_detector = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
 
-def zero_pad(input_img, pad):
-    """    
-    Argument:
-    input_image -- python numpy array of shape (n_H, n_W, n_C)
-    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
-    
-    Returns:
-    input_padded_img -- padded image of shape (n_H + 2*pad, n_W + 2*pad, n_C)
-    """
-
-    input_padded_img = np.pad(input_img, ((pad,pad),(pad,pad),(0,0)), 'constant', constant_values=(0,0))
-    
-    return input_padded_img
-
-'''
-blur = zero_pad(img, 1500)
-plt.imshow(blur)
-'''
- 
-def conv_single_step(img_slice, blur_filter):
-    """  
-    Arguments:
-    img_slice -- slice of input data of shape (f, f, n_C)
-    blur_filter -- filter contained in a window - matrix of shape (f, f, n_C)
-    
-    Returns:
-    Z -- a scalar value, result of convolving the sliding window on a slice x of the input data
-    """
-
-    # Element-wise product then sum between img_slice and blur_filter 
-    Z = np.sum(np.multiply(img_slice, blur_filter))
-    
-    return Z
-    
 def conv_forward(input_img, blur_filter):
     """
     Arguments:
-    input_padded_img -- numpy array of shape (n_H, n_W, n_C)
+    input_img -- numpy array of shape (n_H, n_W, n_C)
     blur_filter -- filter, numpy array of shape (f, f, n_C)
         
     Returns:
@@ -66,8 +34,8 @@ def conv_forward(input_img, blur_filter):
     # Initialize the output volume Z with zeros. 
     Z = np.zeros(shape=(n_H, n_W, n_C))
     
-    # Create A_prev_pad by padding A_prev
-    input_padded_img = zero_pad(input_img, 1)
+    # Create input_padded_img by padding input_img
+    input_padded_img = np.pad(input_img, ((pad,pad),(pad,pad),(0,0)), 'constant', constant_values=(0,0))
 
     for h in range(n_H):                            # loop over vertical axis of the output volume
         for w in range(n_W):                        # loop over horizontal axis of the output volume
@@ -79,16 +47,15 @@ def conv_forward(input_img, blur_filter):
                 horiz_start = w*stride
                 horiz_end = horiz_start+f
                 
-                # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). 
-                a_slice = A[vert_start:vert_end, horiz_start:horiz_end,:]
+                # Use the corners to define the (2D) slice of a_prev_pad. 
+                img_slice = input_padded_img[vert_start:vert_end, horiz_start:horiz_end,c]
                 
-                # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. 
-                Z[h, w, c] = conv_single_step(a_slice, W[:,:,:,c])
+                # Convolve the (2D) slice with the blur filter to get back one output neuron. 
+                # Single step convolution step
+                Z[h, w, c] = np.sum(np.multiply(img_slice, blur_filter))
                                         
-    
-    # Making sure your output shape is correct
-    assert(Z.shape == (n_H, n_W, n_C))
-    
     return Z
 
 blur = conv_forward(img, blur_filter)
+plt.imshow(blur)
+mpimg.imsave('sampleBlur.jpg', blur)
