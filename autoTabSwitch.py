b34a0f8bd7ee8c9d4cb9bde1ea76babc1c89c54d
Sakshamdeep Singh <saksham.sds@gmail.com>
2020-02-14 18:03:08 +0530

    Update autoTabSwitch.py

--- autoTabSwitch.py
+++ autoTabSwitch.py
@@ -8,6 +8,8 @@ Created on Thu Feb 13 18:35:02 2020
 
 import time
 from selenium import webdriver
+import pyautogui
+import random
 
 def setOptions():
     options = webdriver.ChromeOptions()
@@ -30,13 +32,15 @@ def startAutoSwitch():
     # Sensor API Service tab
     driver.get('https://stackoverflow.com/questions/')
     time.sleep(10)          # time needed to enter the password
+    
     '''
     username = driver.find_element_by_id("login_id")
     password = driver.find_element_by_id("login_password")
     username.send_keys("my_username")
-    password.send_keys("my_password")
+    password.send_keys("my_password")  # don't want the password to sit in the script
     driver.find_element_by_name("submit").click()
     '''
+    
     startTab('tab2', 'https://www.cricbuzz.com/')
     startTab('tab3', 'https://www.geeksforgeeks.org/')
     startTab('tab4', 'https://www.youtube.com/')
@@ -47,7 +51,13 @@ def startAutoSwitch():
         for window in Windows:
             driver.refresh()
             driver.switch_to.window(window)
+            moveMouseRandomly()
             time.sleep(15)
+            
+def moveMouseRandomly():        
+    x = random.randint(300, 600)
+    y = random.randint(800, 1000)
+    pyautogui.dragTo(x, y)
         
 if __name__ == '__main__':
     options = setOptions()
